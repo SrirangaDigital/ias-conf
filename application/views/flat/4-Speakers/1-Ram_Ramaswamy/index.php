9763473083b684796ba16b87f8fb293791ca1f0c
shivasdst <shivably04sdst@gmail.com>
2016-09-13 13:54:20 +0530

    No. 17 - Added details of Prof. Ram Ramswamy

---
+++ application/views/flat/4-Speakers/1-Ram_Ramaswamy/index.php
@@ -0,0 +1,28 @@
+<div class="container">
+    <div class="row first-row">
+        <!-- Column 1 -->
+        <div class="col-md-12 text-center">
+            <p>&nbsp;</p>    
+        </div>
+    </div>
+</div>
+<div id="grid" class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+            <p>
+                <img src="<?=PUBLIC_URL?>images/stock/Ram_Ramaswamy.jpg" class="img-thumbnail center-block" alt="Prof. Ram Ramaswamy" />
+                <figcaption class="figure-caption text-center">Prof. Ram Ramaswamy</figcaption>
+            </p>
+        </div>
+        <div class="col-md-3 text-left">
+            <p>
+                <span class="text-bold">Prof. Ram Ramaswamy</span><br />
+                School of Physical Sciences,<br /> 
+                JNU, New Delhi 110 067<br />
+                Tel. Off. (011) 2673 8964 
+                Mobile: 78930 93737<br />
+                Email: r.ramaswamy@gmail.com, ramaswamy@jnu.ac.in
+            </p>
+        </div>    
+    </div>        
+</div>
