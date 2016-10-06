b5edae5104dcbf6cfdd53e69564d9079cbdfe6ac
shivasdst <shivably04sdst@gmail.com>
2016-10-06 19:13:41 +0530

    No. 37 - Updated speakers page

--- application/views/flat/4-Speakers/10-Sanjib_Agarwalla/index.php
+++ application/views/flat/4-Speakers/10-Sanjib_Agarwalla/index.php
@@ -14,7 +14,7 @@
                 <figcaption class="figure-caption text-center">Sanjib K. Agarwalla</figcaption>
             </p>
         </div>
-        <div class="col-md-9 text-left">
+        <div class="col-md-3 text-left">
             <p>
                 <span class="text-bold">Sanjib K. Agarwalla</span><br />
                 Institute of Physics,<br /> 
@@ -23,6 +23,26 @@
                 Tel. Office: (0674) 230 6427;<br /> 
                 Email: sanjib@iopb.res.in, sanjib.agarwalla@gmail.com
             </p>
-        </div>    
+        </div>
+        <div class="col-md-6" id="Sanjib_Agarwalla">
+            <p class="color-event text-bold">Inaugural Lecture</p>
+            <p class="talk">Neutrino oscillation parameters: Present status and future roadmap</p>
+            <p>
+            <span class="abstract">Abstract</span><br />
+            Marvelous world-class experiments involving neutrinos from the Sun, the Earth’s atmosphere,
+            nuclear reactors and accelerators have firmly established neutrino flavour
+            oscillations. This implies that neutrinos have mass and they mix with each other, providing
+            evidence for physics beyond the Standard Model. The most recent development
+            in this field is the discovery of the smallest lepton mixing angle _13. This signifies
+            an important breakthrough in validating the standard three-flavour oscillation picture
+            of neutrinos. In this talk, the present status of the neutrino oscillation parameters will be reviewed, and the scope of addressing the remaining fundamental unknowns,
+            in particular, the type of the neutrino mass hierarchy, the possible presence of a CPviolating
+            phase in the neutrino sector, and octant degeneracy of _23 in current and future
+            long-baseline neutrino oscillation experiments will be discussed. The role of the
+            upcoming India-based Neutrino Observatory (INO) project to resolve the fundamental
+            issue of neutrino mass ordering will be mentioned.
+            </p>
+        </div>            
     </div>        
 </div>
+
--- application/views/flat/4-Speakers/4-Arun_Chattopadhyay/index.php
+++ application/views/flat/4-Speakers/4-Arun_Chattopadhyay/index.php
@@ -14,7 +14,7 @@
                 <figcaption class="figure-caption text-center">Arun Chattopadhyay</figcaption>
             </p>
         </div>
-        <div class="col-md-9 text-left">
+        <div class="col-md-3 text-left">
             <p>
                 <span class="text-bold">Arun Chattopadhyay</span><br />
                 Department of Chemistry,<br /> 
@@ -24,6 +24,18 @@
                 Mobile: 99540 06897;<br /> 
                 Email: arun@iitg.ernet.in
             </p>
-        </div>    
-    </div>        
+        </div> 
+        <div class="col-md-6" id="Arun_Chattopadhyay">
+            <p class="color-event text-bold">Inaugural Lecture</p>
+            <p class="talk">Decorating the surface of a quantum dot with molecules</p>
+            <p>
+                <span class="abstract">Abstract</span><br />
+                Colloidal semiconductor nanocrystals – otherwise known as quantum dots – are emblematic of advanced materials for chemical catalysis, light-emitting devices, medical imaging, solar cells, lasers and electronic devices. Currently the repertoire of quantum dots and knowledge about their properties seem abundant. However, it can be argued that there is still plenty of opportunity for pursuing new chemistry on these small crystals.
+            </p>
+            <p>
+                Our laboratory has introduced the concept of complexation reaction on the surface of the quantum dot. The idea is to pursue the chemical reaction between the labile surface metal ion and an external organic ligand. This results in the formation of inorganic complex on the quantum dot (defined as quantum dot complex) which not only alters the property of the quantum dot but also confers additional properties owing to the complex formed on the surface. The nature of the complex and its number could be tuned depending on the reacting ligands and their concentrations.
+            </p>
+        </div>         
+    </div>    
 </div>
+
