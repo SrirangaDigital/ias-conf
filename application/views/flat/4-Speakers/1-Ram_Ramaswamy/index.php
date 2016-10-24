1a90cbf587a62751a6dc56997fd523456095fa80
shivasdst <shivably04sdst@gmail.com>
2016-10-24 21:23:36 +0530

    Re-organized profiles

--- application/views/flat/4-Speakers/1-Ram_Ramaswamy/index.php
+++ application/views/flat/4-Speakers/1-Ram_Ramaswamy/index.php
@@ -23,6 +23,29 @@
                 Mobile: 78930 93737<br />
                 Email: r.ramaswamy@gmail.com, ramaswamy@jnu.ac.in
             </p>
+            <p class="speakers-profile gap-above-med">Speaker Profile</p>
+            <p>
+            <strong>Ramakrishna Ramaswamy</strong>, School of Physical Sciences, JNU, New Delhi, is President
+            of Indian Academy of Sciences (2016&mdash;18). Ramaswamys research interests lie in the intersection
+            of nonlinear science, statistical physics, and computational biology. He is also a
+            Fellow of the Indian National Science Academy and of The World Academy of Sciences,
+            TWAS.                
+            </p>    
+            <p class="color-event text-bold gap-above-med">Presidential Address</p>
+            <p class="talk">Chimeras: A spontaneous emergence of dynamical disorder</p>
+            <p>
+            <span class="abstract">Abstract</span><br />
+                Networks of identically coupled nonlinear oscillators can in some circumstances spontaneously
+                split into subgroups with different dynamical characteristics. In the simplest examples,
+                there are two subgroups, one of which is fully synchronised while the other is desynchronised;
+                more complex dynamical patterns are possible. Chimeras were first discovered
+                in numerical studies, but have, in the past two decades, been seen in a number of experiments,
+                and are believed to underlie interesting physiological states such as uni-hemispheric
+                sleep that sea mammals and birds experience. Other situations where such states of broken
+                symmetry may be relevant include ventricular fibrillation, and the so-called bump states
+                in neural networks. The origin of the symmetry breaking that gives rise to such complex
+                spatiotemporal patterns and the current interest in these phenomena will be discussed.            
+            </p>
         </div>    
     </div>        
 </div>
--- application/views/flat/4-Speakers/4-Arun_Chattopadhyay/index.php
+++ application/views/flat/4-Speakers/4-Arun_Chattopadhyay/index.php
@@ -14,7 +14,7 @@
                 <figcaption class="figure-caption text-center">Arun Chattopadhyay</figcaption>
             </p>
         </div>
-        <div class="col-md-3 text-left">
+        <div class="col-md-9 text-left">
             <p>
                 <span class="text-bold">Arun Chattopadhyay</span><br />
                 Department of Chemistry,<br /> 
@@ -24,18 +24,34 @@
                 Mobile: 99540 06897;<br /> 
                 Email: arun@iitg.ernet.in
             </p>
-        </div> 
-        <div class="col-md-6" id="Arun_Chattopadhyay">
-            <p class="color-event text-bold">Inaugural Lecture</p>
+            <p class="speakers-profile gap-above-med">Speaker Profile</p>
+            <p>
+                Arun Chattopadhyay, Department of Chemistry, IIT, Guwahati, elected to Fellowship in
+                2016, Chattopadhyay leads a group working on frontier areas of nanoscale science and
+                technology. Recently his group submitted applications for patents on the thumb-imprintbased
+                detection of hyperbilirubinemia and a nanotechnology-based portable machine for
+                pursuing gene and protein analyses. He is the recipient of the Swarnajayanti Fellowship
+                Award (2003-2004), the Materials Research Society of India Medal (2008) and the Young
+                Career Award in Nano Science and Technology (2013).            
+            </p>
+            <p class="color-event text-bold gap-above-med">Inaugural Lecture</p>
             <p class="talk">Decorating the surface of a quantum dot with molecules</p>
             <p>
                 <span class="abstract">Abstract</span><br />
-                Colloidal semiconductor nanocrystals – otherwise known as quantum dots – are emblematic of advanced materials for chemical catalysis, light-emitting devices, medical imaging, solar cells, lasers and electronic devices. Currently the repertoire of quantum dots and knowledge about their properties seem abundant. However, it can be argued that there is still plenty of opportunity for pursuing new chemistry on these small crystals.
+                Colloidal semiconductor nanocrystals otherwise known as quantum dots are emblematic
+                of advanced materials for chemical catalysis, light-emitting devices, medical imaging, solar
+                cells, lasers and electronic devices. Currently the repertoire of quantumdots and knowledge
+                about their properties seem abundant. However, it can be argued that there is still plenty
+                of opportunity for pursuing new chemistry on these small crystals. Our laboratory has
+                introduced the concept of complexation reaction on the surface of the quantum dot. The
+                idea is to pursue the chemical reaction between the labile surface metal ion and an external
+                organic ligand. This results in the formation of inorganic complex on the quantum dot
+                (defined as quantum dot complex) which not only alters the property of the quantum dot
+                but also confers additional properties owing to the complex formed on the surface. The
+                nature of the complex and its number could be tuned depending on the reacting ligands and
+                their concentrations.
             </p>
-            <p>
-                Our laboratory has introduced the concept of complexation reaction on the surface of the quantum dot. The idea is to pursue the chemical reaction between the labile surface metal ion and an external organic ligand. This results in the formation of inorganic complex on the quantum dot (defined as quantum dot complex) which not only alters the property of the quantum dot but also confers additional properties owing to the complex formed on the surface. The nature of the complex and its number could be tuned depending on the reacting ligands and their concentrations.
-            </p>
-        </div>         
+        </div> 
     </div>    
 </div>
 
