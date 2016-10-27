e79bd62825a763aab2945f68126b7704705fada3
shivasdst <shivably04sdst@gmail.com>
2016-10-27 18:13:51 +0530

    created sidebar containing quick links

---
+++ application/views/generalSidebar.php
@@ -0,0 +1,34 @@
+    <h1>Quick links</h1>
+
+    <h1>Schedule</h1>
+    <ul>
+        <li><a href="<?=BASE_URL?>page/flat/Schedule#day1" target="_blank">Day1</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Schedule#day2" target="_blank">Day2</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Schedule#day3" target="_blank">Day3</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Schedule#allied" target="_blank">Allied Events</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Schedule#spouse" target="_blank">For Spouses</a></li>
+    </ul>            
+
+    <h1>For Participants</h1>
+    <ul>
+        <li><a href="http://web-japps.ias.ac.in:8080/Annualmeeting2016/annual/register/Register.jsp" target="_blank">Registration</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Information/Venue" target="_blank">Venue</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Information/Accommodation" target="_blank">Accomodation</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Information/Transport" target="_blank">Transport</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Information/Contact" target="_blank">Contact</a></li>
+        <li><a href="<?=BASE_URL?>page/flat/Information/Other" target="_blank">Other Information</a></li>
+    </ul>    
+
+    <h1>Downloads</h1>
+    <ul>
+        <li><a href="<?=DOWNLOAD_URL?>2016AMabstractcoverprn.pdf" target="_blank">Cover page</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>Abstract-contents-final.pdf" target="_blank">Contents page</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>AM2016-abstracts-final.pdf" target="_blank">Speaker Profiles and Abstracts</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>AMcardprn22oct2016.pdf" target="_blank">Programmes Card</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>AMprogposterprn22oct2016.pdf" target="_blank">Poster of Programmes</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>AMlectposterprn22oct2016.pdf" target="_blank">Poster of Special Lectures</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>AMsympposterprn22oct2016.pdf" target="_blank">Poster of Symposiums</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>A_Life_in_ScienceInviteside_1.jpg" target="_blank">Book release poster</a></li>
+        <li><a href="<?=DOWNLOAD_URL?>A_Life_in_ScienceInviteside_2.jpg" target="_blank">Book release invitation</a></li>
+        <li><a href="<?=BASE_URL?>Information/Venue#bhopal_campus_map" target="_blank">IISER Bhopal - Campus plan</a></li>
+    </ul>
